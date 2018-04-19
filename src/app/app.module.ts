12d9fc7baa22f9d8de9f0dafea93b29d7a117e7e
ravi-reglobe <ravi.k@cashify.in>
2018-04-19 11:28:49 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
+import {ServiceWorkerModule, SwUpdate} from '@angular/service-worker';
+import {AppComponent} from './app.component';
 
-import { environment } from '../environments/environment';
+import {environment} from '../environments/environment';
+import {UpdateAvailableEvent} from '@angular/service-worker/src/low_level';
 
 @NgModule({
   declarations: [
@@ -13,9 +14,32 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private swUpdate: SwUpdate) {
+    this.checkForUpdate();
+  }
+
+  private checkForUpdate() {
+    if (this.swUpdate.isEnabled) {
+
+      this.swUpdate.available.subscribe((event: UpdateAvailableEvent) => {
+
+        console.log('current version', event.current.appData['version'], ',', event.available.appData['version']);
+        console.log('new version', event.available.appData['version']);
+
+        if (confirm('New version available. Load New Version?')) {
+          window.location.reload();
+        }
+      });
+      this.swUpdate.checkForUpdate().then(() => {
+        console.log('this.swUpdate.checkForUpdate()');
+      });
+    }
+  }
+}
